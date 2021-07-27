719c4c80b526c095df9d9986961cc73de6d6d0ee
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-07-27 15:57:15 +0200

    Exercise 9 - WIP

--- expert.php
+++ expert.php
@@ -142,3 +142,25 @@ echo login('john@example.be', 'dfgidfgdfg');
 //Should say: no access
 echo login('wrong@example', 'wrong');
 /* You can change code again */
+
+
+new_exercise(9);
+function isLinkValid(string $link) {
+    $unacceptables = array('https:','.doc','.pdf', '.jpg', '.jpeg', '.gif', '.bmp', '.png');
+
+    foreach ($unacceptables as $unacceptable) {
+        if (strpos($link, $unacceptable) == true) {
+            return 'Unacceptable Found<br />';
+        }
+    }
+    return 'Acceptable<br />';
+}
+//invalid link
+echo isLinkValid('http://www.google.com/hack.pdf');
+//invalid link
+echo isLinkValid('https://google.com');
+//VALID link
+echo isLinkValid('http://google.com');
+//VALID link
+echo isLinkValid('http://google.com/test.txt');
+
