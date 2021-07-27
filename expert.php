3df740ae984f4675e2f2a68b223cb049084545c4
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-07-27 15:05:50 +0200

    Complete exercise 7

--- expert.php
+++ expert.php
@@ -115,3 +115,13 @@ function randomHeroName()
 }
 
 echo "Here is the name: " . combineNames();
+
+
+
+new_exercise(7);
+function copyright(int $year) {
+    return "&copy; $year BeCode";
+}
+//print the copyright
+echo copyright((int) date("Y"));
+
