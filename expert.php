1a0c15658712c13d2392b695dbfedbde1f722c2a
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-07-27 11:23:42 +0200

    Complete exercise 2 & 3

--- expert.php
+++ expert.php
@@ -11,8 +11,34 @@ echo "Exercise 1 starts here:";
 
 function new_exercise($x) {
 
-    $block = "<br/><hr/><br/><br/>Exercise {$x} starts here:<br/>";
+    $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
     echo $block;
 }
 
+
+
+
 new_exercise(2);
+
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+
+echo $monday;
+
+
+
+
+new_exercise(3);
+// === Exercise 3 ===
+// This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
+
+$str = "Debugged ! Also very fun";
+echo substr($str, 0, 10);
+
+
+
+
